b2b6e856bbc0127f37b29e14826309cec66f4370
Syed Usman Ahmed <usmanrdx903@gmail.com>
2026-06-14 11:13:11 +0500

    added robot.js and sitemap.js files

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://syedusmanahmed.vercel.app/sitemap.xml',
+  };
+}
